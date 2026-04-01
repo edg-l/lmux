d41e0d2d2524a726c6bb70baa0420e4e36e0399b
Edgar Luque <git@edgl.dev>
2026-04-01 11:35:14 +0200

    Add kv_cache_dir to allowed settings keys

--- src/routes/api/settings/+server.ts
+++ src/routes/api/settings/+server.ts
@@ -27,7 +27,14 @@ export const GET: RequestHandler = async () => {
 	});
 };
 
-const ALLOWED_KEYS = new Set(['models_dir', 'llama_server_path', 'hf_token', 'tools_enabled', 'searxng_url']);
+const ALLOWED_KEYS = new Set([
+	'models_dir',
+	'llama_server_path',
+	'hf_token',
+	'tools_enabled',
+	'searxng_url',
+	'kv_cache_dir'
+]);
 
 export const PUT: RequestHandler = async ({ request }) => {
 	const body = (await request.json()) as { key: string; value: string };
